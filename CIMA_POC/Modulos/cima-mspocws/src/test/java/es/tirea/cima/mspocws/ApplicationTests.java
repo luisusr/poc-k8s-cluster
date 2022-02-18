4064dee645f766ec4777e4098bdca026e4c890ff
Luis Batista (luisusrws) <luisarturo.batista@mgt-consulting.com>
2022-02-18 18:55:14 +0100

    Migracion y cambios faltantes desde repositorio gitlab queda eliminado. Se continua mantenimiento desde este repo

--- CIMA_POC/Modulos/cima-mspocws/src/test/java/es/tirea/cima/mspocws/ApplicationTests.java
+++ CIMA_POC/Modulos/cima-mspocws/src/test/java/es/tirea/cima/mspocws/ApplicationTests.java
@@ -2,8 +2,10 @@ package es.tirea.cima.mspocws;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class ApplicationTests {
 
 	@Test
